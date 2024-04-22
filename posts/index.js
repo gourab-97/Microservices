6aaa277e62826a88c43bc42fc4aedd468e6150f0
Gourab Saha <gourabsaha667@gmail.com>
2024-04-22 20:37:24 +0530

    New Changes
    New Changes 1

--- posts/index.js
+++ posts/index.js
@@ -1,42 +1,45 @@
-const express=require("express");
-const app=express();
-const {randomBytes}=require('crypto');
-const cors=require('cors');
-const bodyParser=require('body-parser');
-const axios=require('axios');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
+const axios = require("axios");
 
+const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const posts={};
+const posts = {};
 
-app.get('/posts',(req,res)=>{
-    res.send(posts);
+app.get("/posts", (req, res) => {
+  res.send(posts);
 });
 
-app.post('/posts',async(req,res)=>{
-    const id=randomBytes(4).toString('hex');
-    const {title}=req.body;
+app.post("/posts", async (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
 
-    posts[id]={
-        id,title
-    };
+  posts[id] = {
+    id,
+    title,
+  };
 
-    await axios.post('http://localhost:4005/events',{
-        type:'PostCreated',
-        data:{
-            id,title
-        }
-    });
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: {
+      id,
+      title,
+    },
+  });
 
-    res.status(201).send(posts[id]);
+  res.status(201).send(posts[id]);
 });
 
-app.post('/events',(req,res)=>{
-    console.log('Recieved event',req.body.type);
-    res.send({});
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body.type);
+
+  res.send({});
 });
 
-app.listen(4000,()=>{
-    console.log('Listening on 4000');
+app.listen(4000, () => {
+  console.log("Listening on 4000");
 });
